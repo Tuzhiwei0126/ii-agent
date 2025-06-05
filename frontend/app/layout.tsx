a7d3d6906bfac7b86dbd490827663f3a46ecc6af
涂志伟 <799214697@qq.com>
2025-06-05 14:52:19 +0800

    Feat: 优化logo

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -8,8 +8,8 @@ import { ThemeProvider } from '@/components/theme-provider';
 import 'antd/dist/reset.css';
 
 export const metadata: Metadata = {
-  title: "II Agent",
-  description: "Intelligent Interface Agent",
+  title: "GoAgent",
+  description: "GoAgent",
 };
 
 export default function RootLayout({
@@ -19,26 +19,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="zh-CN" suppressHydrationWarning>
-      <head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicon/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-      </head>
       <body className={`antialiased`}>
         <ThemeProvider>
           <AuthGuard>
